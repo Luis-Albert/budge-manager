1c0aebd7c412c1280549c09b34b93ab287c8e8a0
Luis21 <huamanluis808@gmail.com>
2022-12-09 11:28:56 -0500

    Agregando Header
    Se agrega Header y normalize en el index, tambien se agrega el style font

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
-import React from "react";
+import { Header } from "./components/Header";
 
 export const App = () => {
-  return <div>Hello world</div>;
+  return (
+    <Header />
+  )
 };
---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export const Header = () => {
+  return (
+
+    <header>
+      <h1>Planificador de gastos</h1>
+    </header>
+
+  )
+};
